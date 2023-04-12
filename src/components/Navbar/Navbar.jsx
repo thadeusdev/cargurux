c16cba5b8d4d03d7f5c169b547b59a9f18544d04
thadeus <thadeusnyariki94@gmail.com>
2023-04-13 02:08:13 +0300

    top

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 import './Navbar.scss'
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import PhoneIcon from '@mui/icons-material/Phone';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className="top">
+        <div className="left">
+          <img src={require('../../images/logo.png')} alt="" />
+        </div>
+        <div className="center">
+          <LocationOnIcon className='icon'/>
+          <span>pangani Next to ABC Mosque</span>
+        </div>
+        <div className="right">
+          <div className="phone">
+            <PhoneIcon className='icon'/>
+            <span>+254759272383</span>
+          </div>
+          <div className="social">
+            <FacebookIcon className='facebook' /> 
+            <TwitterIcon className='twitter' /> 
+            <InstagramIcon className='instagram' />   
+            <LinkedInIcon className='linkedin' /> 
+            <AccountCircleIcon className='account' />      
+          </div>
+        </div>
+      </div>
+      <div className="bottom">bottom</div>
+    </div>
   )
 }
 
