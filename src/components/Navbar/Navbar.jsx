a77398a022e58d0e8301ee9585abcf8be0d868f8
thadeus <thadeusnyariki94@gmail.com>
2023-04-13 02:53:24 +0300

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,6 +7,7 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import PhoneIcon from '@mui/icons-material/Phone';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
@@ -33,7 +34,31 @@ const Navbar = () => {
           </div>
         </div>
       </div>
-      <div className="bottom">bottom</div>
+      <div className="bottom">
+        <div className="links">
+          <Link className='link-item' to='/'>
+            <span>HOME</span>
+          </Link>
+          <Link className='link-item' to='/cars-for-sale'>
+            <span>CAR FOR SALE</span>
+          </Link>
+          <Link className='link-item' to='/car-accessories'>
+            <span>CAR ACCESSORIES</span>
+          </Link>
+          <Link className='link-item' to='/car-tracking'>
+            <span>CAR TRACKING</span>
+          </Link>
+          <Link className='link-item' to='/blog-listing'>
+            <span>BLOGS</span>
+          </Link>
+          <Link className='link-item' to='/vehicle-insurance'>
+            <span>INSURANCE</span>
+          </Link>
+          <Link className='link-item' to='/about-us'>
+            <span>ABOUT US</span>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
