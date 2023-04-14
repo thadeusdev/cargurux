00f34abbc0f8a60c06051ac593da4a8ce4e91346
thadeus <thadeusnyariki94@gmail.com>
2023-04-14 06:05:57 +0300

    weekcars

--- src/components/WeeksCars/WeeksCar.jsx
+++ src/components/WeeksCars/WeeksCar.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import './WeeksCar.scss'
 import { weeksCars } from '../../data'
+import SpeedIcon from '@mui/icons-material/Speed';
+import SettingsIcon from '@mui/icons-material/Settings';
+import LocalGasStationIcon from '@mui/icons-material/LocalGasStation';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import PhoneIcon from '@mui/icons-material/Phone';
 
 const WeeksCar = () => {
     // console.log(weeksCars)
@@ -20,11 +25,25 @@ const WeeksCar = () => {
             {weeksCars.map(car => (
                 <div className='bottom-item'>
                     <img src={car.img} alt="" />
-                    <span className="desc">{car.desc}</span>
-                    <span className="desc">Ksh {car.price}</span>
-                    <span className="mileage">{car.engine} Cc</span> 
-                    <span className="desc">{car.transmission}</span>
-                    <div className="order">order</div>                   
+                    <span className="desc" style={{fontWeight: 600, padding: '5px'}}>{car.desc}</span>
+                    <span className="desc" style={{color: 'red', fontWeight: 600, marginBottom: '10px', padding: '5px'}}>Ksh {car.price}</span>
+                    <div className="mileage">
+                        <SpeedIcon/>
+                        <span className="mileage-item">{car.engine} Cc</span>
+                        <SettingsIcon/> 
+                        <span className="mileage-item">{car.transmission}</span>
+                        <LocalGasStationIcon/>
+                        <span className="mileage-item">{car.fuel}</span>
+                    </div>
+                    <div className="order">
+                        <div className="left">
+                            <button>order now</button>
+                        </div>
+                        <div className="right">
+                            <WhatsAppIcon className='whatsapp'/>
+                            <PhoneIcon className='phone'/>
+                        </div>
+                    </div>                   
                 </div>
             ))}
             
