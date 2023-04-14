d3dcee875f0de83bf9934969ac3d8ec606508936
thadeus <thadeusnyariki94@gmail.com>
2023-04-14 07:11:14 +0300

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,24 @@ import './Footer.scss'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className="top">
+        <div className="subscribe">
+          <h2>subscribe to our newsletter to get update</h2>
+          <form action="">
+            <input type="text" placeholder='Email Address' />
+            <button>subscribe</button>
+          </form>
+        </div>
+        <div className="links">Links</div>
+        <div className="support">support</div>
+        <div className="about">about</div>
+      </div>
+      <div className="bottom">
+        <span>© 2023 Carguruz Ltd. All Rights Reserved</span>
+        <span>site hosted by heroku</span>
+      </div>
+    </div>
   )
 }
 
