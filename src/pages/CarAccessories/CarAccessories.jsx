42d8520bebf17be61b06f6f536d873bcb5366f89
thadeus <thadeusnyariki94@gmail.com>
2023-04-15 23:30:56 +0300

    right

--- src/pages/CarAccessories/CarAccessories.jsx
+++ src/pages/CarAccessories/CarAccessories.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './CarAccessories.scss'
+import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled';
 
 const CarAccessories = () => {
   return (
@@ -21,9 +22,70 @@ const CarAccessories = () => {
         <div className="right">
           <div className="header">
             <div className="highlight"></div>
+            <AccessTimeFilledIcon/>
             <h2>hours of <span>operation</span></h2>
           </div>
-          <div className="table"></div>
+          <table className='table'>
+            <thead>
+              <tr>
+                <th>sunday</th>
+              </tr>
+              <hr />
+              <tr>
+                <th>monday</th>
+              </tr>
+              <hr />
+              <tr>
+                <th>tuesday</th>
+              </tr>
+              <hr />
+              <tr>
+                <th>wednesday</th>
+              </tr>
+              <hr />
+              <tr>
+                <th>thursday</th>
+              </tr>
+              <hr />
+              <tr>
+                <th>friday</th>
+              </tr>
+              <hr />
+              <tr>
+                <th>saturday</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>9:00 am - 6:00 pm</td>
+              </tr>
+              <hr />
+              <tr>
+                <td>8:00 am - 6:00 pm</td>
+              </tr>
+              <hr />
+              <tr>
+                <td>8:00 am - 6:00 pm</td>
+              </tr>
+              <hr />
+              <tr>
+                <td>8:00 am - 6:00 pm</td>
+              </tr>
+              <hr />
+              <tr>
+                <td>8:00 am - 6:00 pm</td>
+              </tr>
+              <hr />
+              <tr>
+                <td>8:00 am - 6:00 pm</td>
+              </tr>
+              <hr />
+              <tr>
+                <td>9:00 am - 6:00 pm</td>
+              </tr>
+            </tbody>
+          </table>
+          <button>+254 759 272 383</button>
         </div>
       </div>
       <div className="bottom">bottom</div>
