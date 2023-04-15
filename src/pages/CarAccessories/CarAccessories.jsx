2e8d72b9cd3cb6f5424ead1c7ae7269a3c237e2b
thadeus <thadeusnyariki94@gmail.com>
2023-04-15 22:14:47 +0300

    left

--- src/pages/CarAccessories/CarAccessories.jsx
+++ src/pages/CarAccessories/CarAccessories.jsx
@@ -3,7 +3,31 @@ import './CarAccessories.scss'
 
 const CarAccessories = () => {
   return (
-    <div>CarAccessories</div>
+    <div className='carAccessories'>
+      <div className="top">
+        <div className="left">
+          <div className="header">
+            <div className="highlight"></div>
+            <h2>facelift <span>services</span></h2>
+          </div>          
+          <div className="image-wrapper">
+            <img src={require('../../images/facelift.jpg')} alt="" />
+            <hr />
+            <span>
+              It Does not matter the Year, the Make or what you don't like about your current car We can make your Car look current, feel current and Be Current.
+            </span>
+          </div>
+        </div>
+        <div className="right">
+          <div className="header">
+            <div className="highlight"></div>
+            <h2>hours of <span>operation</span></h2>
+          </div>
+          <div className="table"></div>
+        </div>
+      </div>
+      <div className="bottom">bottom</div>
+    </div>
   )
 }
 
