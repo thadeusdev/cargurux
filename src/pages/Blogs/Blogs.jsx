dc37b6bdf24c715d1445c20390541c5e1cbbda8e
thadeus <thadeusnyariki94@gmail.com>
2023-04-17 15:45:57 +0300

    blog

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -3,7 +3,21 @@ import './Blogs.scss'
 
 const Blogs = () => {
   return (
-    <div>Blogs</div>
+    <div className='blogs'>
+      <div className="left">
+        <div className="header">
+          <div className="highlight"></div>
+          <h2>Get your unbeatable car financing option with us today</h2>
+        </div>
+        <span>
+          We make it easier for you to buy the car of your dream with low interest rates, flexible payment terms and fast approval times.
+        </span>
+        <button>find your dream car</button>
+      </div>
+      <div className="right">
+        <img src={require('../../images/car_financing_hero_image.webp')} alt="" />
+      </div>
+    </div>
   )
 }
 
